63b2c4f8f3cbb36faa56b0f894500f401363d49c
Gino Cova <gcova.1249@unimar.edu.ve>
2024-11-16 00:56:16 -0400

    agregado de variables

--- bingo.pas
+++ bingo.pas
@@ -2,4 +2,10 @@ program bingo;
 
 type
     carton = array[1..5,1..5] of Integer;
-    
+    cartonCheck = array[1..5,1..5] of Boolean;
+jugador = record
+jugador: String;
+ cartones : array of carton;
+  verificadores : array of cartonCheck;
+end;
+jugadores = array of Jugador;
