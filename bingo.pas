01fe82055f7805af71b07c69a92a5829de24ea71
Ginosaurio04 <gcova.1249@unimar.edu.ve>
2024-11-20 15:18:31 -0400

    mejora en los arreglos

--- bingo.pas
+++ bingo.pas
@@ -1,16 +1,12 @@
 program bingos;
 
 type
-    carton = array[1..5,1..5] of Integer;
-    cartonCheck = array[1..5,1..5] of Boolean;
-jugador = record
-jugador: String;
- cartones : array of carton;
-  verificadores : array of cartonCheck;
-end;
-jugadores = array of Jugador;
+    carton : array[1..5,1..5] of Integer;
+    catonbool:array[1..5,1..5] to boolean;
+cartones: array of cartón;
+jugadores: array of cartones 
 var
 bingo: jugadores;
 begin
   
-end;
+end.
